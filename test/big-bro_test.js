a310573584a91c69f53fd693b5bff4d9868d715b
coffutt <coffutt@paypal.com>
2015-01-12 20:02:23 -0500

    First commit

---
+++ test/big-bro_test.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var expect = require('chai').expect,
+    bigBro = require('../lib/big-bro.js');
+
+describe('Big Bro', function () {
+
+    var bro,
+        littleBro;
+
+    before(function () {
+        littleBro = {
+            a: 'A',
+            b: 'B',
+            c: {
+                d: 'D',
+                e: 'E'
+            }
+        };
+    });
+
+    // it('should build a bro', function () {
+    //     var bro = bigBro({ obj: littleBro });
+    //     expect(bro.suspend).to.be.a.Function;
+    //     expect(bro.resume).to.be.a.Function;
+    //     expect(bro.addListener).to.be.a.Function;
+    //     expect(bro.clearListeners).to.be.a.Function;
+    // });
+
+    it('should fire off event listeners when little bro changes', function (done) {
+        var numChanges = 0;
+
+        var bro = bigBro({
+            obj: littleBro,
+            callbacks: function (current) {
+                var expected = {
+                    1: {
+                        a: 'F',
+                        b: 'B',
+                        c: {
+                            d: 'D',
+                            e: 'E'
+                        }
+                    },
+                    2: {
+                        a: 'F',
+                        b: 'B',
+                        c: {
+                            d: 'G',
+                            e: 'E'
+                        }
+                    }
+                };
+
+                expect(expected[numChanges]).to.deep.equal(current);
+
+                if (numChanges === 2){
+                    done();
+                }
+            }
+        });
+
+        numChanges++;
+        littleBro.a = 'F';
+        numChanges++;
+        littleBro.d = 'G';
+    });
+
+});
