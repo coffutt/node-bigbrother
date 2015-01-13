9b6e9200b5487c06cf4ec5759a420afb4a438be0
coffutt <coffutt@paypal.com>
2015-01-12 21:03:39 -0500

    Fixed the bubbling problem

--- test/big-bro_test.js
+++ test/big-bro_test.js
@@ -19,17 +19,14 @@ describe('Big Bro', function () {
         };
     });
 
-    // it('should build a bro', function () {
-    //     var bro = bigBro({ obj: littleBro });
-    //     expect(bro.suspend).to.be.a.Function;
-    //     expect(bro.resume).to.be.a.Function;
-    //     expect(bro.addListener).to.be.a.Function;
-    //     expect(bro.clearListeners).to.be.a.Function;
-    // });
+    it('should build a bro', function () {
+        var bro = bigBro({ obj: littleBro });
+        expect(bro.suspend).to.be.a.Function;
+        expect(bro.resume).to.be.a.Function;
+        expect(bro.addListener).to.be.a.Function;
+        expect(bro.clearListeners).to.be.a.Function;
+    });
 
-    // Note that this fails because the temp objects don't bubble up properly. We detect
-    // the change but we're unable to pass back the fully updated object when sub resources
-    // are recieving the updates. 
     it('should fire off event listeners when little bro changes', function (done) {
         var numChanges = 0;
 
@@ -65,7 +62,6 @@ describe('Big Bro', function () {
         numChanges++;
         littleBro.a = 'F';
         numChanges++;
-
         littleBro.c.d = 'G';
     });
 
